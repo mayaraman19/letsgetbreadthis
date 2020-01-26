372799a745d52c48b4c51c8460f163d9c08cd6ce
Damini Kaushik <daminik@wifi-131-179-48-128.host.ucla.edu>
2020-01-26 12:20:35 -0800

    fix redirecting

--- src/Register.js
+++ src/Register.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
 import './App.css';
 import firebase from './lib/firebase';
 import {useEffect, useState} from 'react';
 import Person from './Person';
+import {Redirect} from 'react-router-dom'
+
 
 class Register extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             name: "",
+            age: "",
             school: "",
-            age: ""
+            year: "",
+            pronouns: "",
+            interests: "",
+            toContacts: false
         };
       }
-
+    
     handleNameChange = e => {
         this.setState({ name: e.target.value });
     }
@@ -26,7 +34,39 @@ class Register extends React.Component{
         this.setState({ age: e.target.value });
     }
 
-    render = () => {
+    handleYearChange = e => {
+      console.log(e);
+        this.setState({ year: e.value});
+    }
+
+    handlePronounsChange = e => {
+        this.setState({ pronouns: e.target.value});
+    }
+
+    handleInterestsChange = e => {
+        this.setState({ interests: e.target.value});
+    }
+    submitForm = () => {
+    let db = firebase.firestore();
+    console.log(this.state);
+    db.collection("users-test").add(this.state)
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+      this.setState({toContacts: true});
+    }
+
+    render() {
+      const options = [
+        'Freshman', 'Sophomore', 'Junior', 'Senior', 'Other'
+      ];
+      const defaultOption = options[0];
+      if (this.state.toContacts === true) {
+        return <Redirect to='/users' />
+      }
         return (
             <div>
             <p>Name:</p>
@@ -35,22 +75,36 @@ class Register extends React.Component{
                 value={this.state.name} 
                 onChange={this.handleNameChange}
             />
+            <p>Age:</p>
+            <input 
+                type="number"
+                value={this.state.age} 
+                onChange={this.handleAgeChange}
+            />
             <p>School:</p>
             <input 
                 type="text"
                 value={this.state.school} 
                 onChange={this.handleSchoolChange}
             />
-            <p>Age:</p>
-            <input 
-                type="text"
-                value={this.state.age} 
-                onChange={this.handleAgeChange}
+            <p>Year:</p>
+            <Dropdown options={options} onChange={this.handleYearChange} value={this.state.year} placeholder="Select an option" />
+            <p>Pronouns</p>
+            <input
+              type="text"
+              value={this.state.pronouns}
+              onChange={this.handlePronounsChange}
+            />
+            <p>Interests</p>
+            <input
+              type="text"
+              value={this.state.interests}
+              onChange={this.handleInterestsChange}
             />
             <br></br>
             <br></br>
             <button 
-            onClick={this.createMessage}
+            onClick={this.submitForm}
             >
                 Submit
             </button>
