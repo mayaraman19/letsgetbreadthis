8048661b3170a96d8d897962d168b337efbf17d0
Maya Raman <MayaRaman@wifi-131-179-3-110.host.ucla.edu>
2020-01-26 00:56:41 -0800

    button stuff

---
+++ src/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import firebase from './lib/firebase';
+import {useEffect, useState} from 'react';
+import Person from './Person';
+
+
+function Register() {
+    return (
+        
+    )
+}
