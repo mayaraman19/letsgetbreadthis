dc965c0d98beddcb90f63bd822406d42b16342f7
jyeh20 <jmyeh51@gmail.com>
2020-01-26 10:48:24 -0800

    Added Year, Pronouns, Interests

--- src/Register.js
+++ src/Register.js
@@ -10,8 +10,11 @@ class Register extends React.Component{
         super(props);
         this.state = {
             name: "",
+            age: "",
             school: "",
-            age: ""
+            year: "",
+            pronouns: "",
+            Interests: ""
         };
       }
     
@@ -27,6 +30,18 @@ class Register extends React.Component{
         this.setState({ age: e.target.value });
     }
 
+    handleYearChange = e => {
+        this.setState({ age: e.target.value});
+    }
+
+    heandlePronounsChange = e => {
+        this.setState({ age: e.target.value});
+    }
+
+    handleInterestsChange = e => {
+        this.setState({ age: e.target.value});
+    }
+
     render() {
         return (
             <div>
@@ -36,17 +51,35 @@ class Register extends React.Component{
                 value={this.state.name} 
                 onChange={this.handleNameChange}
             />
+            <p>Age:</p>
+            <input 
+                type="number"
+                value={this.state.age} 
+                onChange={this.handleAgeChange}
+            />
             <p>School:</p>
             <input 
                 type="text"
                 value={this.state.school} 
                 onChange={this.handleSchoolChange}
             />
-            <p>Age:</p>
-            <input 
-                type="text"
-                value={this.state.age} 
-                onChange={this.handleAgeChange}
+            <p>Year:</p>
+            <input
+              type="number"
+              value={this.state.year}
+              onChange={this.handleSchoolChange}
+            />
+            <p>Pronouns</p>
+            <input
+              type="text"
+              value={this.state.pronouns}
+              onChange={this.handlePronounsChange}
+            />
+            <p>Interests</p>
+            <input
+              type="text"
+              value={this.state.interests}
+              onChange={this.handlePronounsChange}
             />
             <br></br>
             <br></br>
