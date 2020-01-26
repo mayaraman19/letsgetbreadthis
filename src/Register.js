7bd9924455edf2b552f38390cae31485954e2fbc
Damini Kaushik <daminik@wifi-131-179-48-128.host.ucla.edu>
2020-01-26 10:24:02 -0800

    wkejganl

--- src/Register.js
+++ src/Register.js
@@ -5,42 +5,18 @@ import {useEffect, useState} from 'react';
 import Person from './Person';
 
 
-<<<<<<< HEAD
 class Register extends React.Component{
-    cconstructor(props) {
-        super(props);
-        this.state = { username: '' };
-      }
-      myChangeHandler = (event) => {
-        this.setState({username: event.target.value});
-      }
-      render() {
+    render() {
         return (
-          <form>
-          <h1>Hello {this.state.username}</h1>
-          <p>Enter your name:</p>
-          <input
-            type='text'
-            onChange={this.myChangeHandler}
-          />
-          </form>
+            <form>
+            <h1>Hello</h1>
+            <p>Enter your name:</p>
+            <input
+            type="text"
+            />
+        </form>
         );
-      }
+    }
 }
 
 export default Register;
-=======
-function Register (){
-    return ( 
-        <form>
-        <h1>Hello</h1>
-        <p>Enter your name:</p>
-        <input
-          type="text"
-        />
-        </form>
-    );
-    }
-  
-  export default Register;
->>>>>>> 866b35b4980ab5359247e0f765ef839585e13ebe
