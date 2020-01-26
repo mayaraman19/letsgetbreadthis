7c1ab038ce4bcf03aa7ee5d3442d5cf6caa6d114
Damini Kaushik <daminik@wifi-131-179-48-128.host.ucla.edu>
2020-01-26 10:34:02 -0800

    add form

--- src/Register.js
+++ src/Register.js
@@ -6,15 +6,54 @@ import Person from './Person';
 
 
 class Register extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "",
+            school: "",
+            age: -1
+        };
+      }
+    
+    handleNameChange = e => {
+        this.setState({ name: e.target.value });
+    }
+    
+    handleSchoolChange = e => {
+        this.setState({ school: e.target.value });
+    }
+
+    handleAgeChange = e => {
+        this.setState({ age: e.target.value });
+    }
+
     render() {
         return (
-            <form>
-            <h1>Hello</h1>
-            <p>Enter your name:</p>
-            <input
-            type="text"
+            <div>
+            <p>Name:</p>
+            <input 
+                type="text"
+                value={this.state.name} 
+                onChange={this.handleNameChange}
             />
-        </form>
+            <p>School:</p>
+            <input 
+                type="text"
+                value={this.state.school} 
+                onChange={this.handleSchoolChange}
+            />
+            <Age>Age:</Age>
+            <input 
+                type="number"
+                value={this.state.age} 
+                onChange={this.handleAgeChange}
+            />
+            <button 
+            onClick={this.createMessage}
+            >
+                Send Message
+            </button>
+            </div>
         );
     }
 }
